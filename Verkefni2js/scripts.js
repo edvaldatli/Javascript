0cef72fabfd684383b8411bd6e1feef98b177113
edvaldatli <eddi1122@hotmail.com>
2016-02-01 12:03:04 +0000

    Update 1.2

--- Verkefni2js/scripts.js
+++ Verkefni2js/scripts.js
@@ -8,22 +8,43 @@ var pizzasmidur = function(obj, Nafn, Alegg, Verd, Stærð, divNafn){
         verd: Verd,
         stærð: Stærð
     }
+    nafn = obj.nafn;
+    alegg = obj.alegg;
+    verd = obj.verd;
+    stærð = obj.stærð;
+
     var iDiv = document.createElement('div');
-    iDiv.id = 'block';
-    iDiv.className = 'block';
-
-// Create the inner div before appending to the body
-    var innerDiv = document.createElement('div');
-    innerDiv.className = 'block-2';
-
-// The variable iDiv is still good... Just append to it.
-    iDiv.appendChild(innerDiv);
+    iDiv.id = divNafn;
 
 // Then append the whole thing onto the body
     document.getElementsByTagName('body')[0].appendChild(iDiv);
+
+    var pizzatexti = document.createElement('span')
+    document.getElementsByTagName('body')[0].appendChild(pizzatexti);
+    pizzatexti.innerHTML = nafn;
+
+    pizzatexti = document.createElement('span')
+    document.getElementsByTagName('body')[0].appendChild(pizzatexti);
+    pizzatexti.innerHTML = alegg;
+
+    pizzatexti = document.createElement('span')
+    document.getElementsByTagName('body')[0].appendChild(pizzatexti);
+    pizzatexti.innerHTML = verd + " kr";
+
+    pizzatexti = document.createElement('span');
+    document.getElementsByTagName('body')[0].appendChild(pizzatexti);
+    pizzatexti.innerHTML = stærð;
 }
 
 
 
-pizzasmidur('pizza1', 'Pepperoni Veisla', 'Pepperoni', 2090, 'Stór');
+pizzasmidur('pizza1', 'Pepperoni Veisla', 'Pepperoni', 2090, 'Stór', 'pepperoni');
+pizzasmidur('pizza1', 'Pepperoni Veisla', 'Pepperoni', 2090, 'Stór', 'pepperoni');
+
+
+
+
+
+
+
 
