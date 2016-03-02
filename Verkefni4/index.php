a25a3060e57be5bd84492c0e4b2ea1b3b97f1424
edvaldatli <eddi1122@hotmail.com>
2016-03-02 10:09:41 +0000

    Yee

---
+++ Verkefni4/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <div id="main">
+
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script type="text/javascript" src="js/quiz.js"></script>
+</html>
---
+++ Verkefni4/js/quiz.js
@@ -0,0 +1,31 @@
+function Spurning (spurning, svor, rettsvar){
+    this.spurning = spurning,
+    this.svor = svor,
+    this.rettsvar = rettsvar
+
+    this.template = function(){
+
+        var spurning = '<div class="spurning">' + '<h3>' + this.spurning + '</h3>';
+        var ehv = "";
+        for(var i = 0; i < 4;i++){
+             ehv += "<button id='svar'>" + this.svor[i] + "</button>";
+        }
+
+        var last = '</div>';
+
+        return spurning + ehv + last;
+    }
+}
+
+var spurningar = [
+    new Spurning('What is Elvis Presley s middle name?', ['Michael', 'Aaron', 'Keith', 'Johnny'], 1),
+    new Spurning('How many oscars did the Titanic movie got?', [9, 4, 7, 11], 11),
+    new Spurning('In which country were the first Olympic Games held?', ['United Kingdom', 'France', 'Greece', 'Czech Republic'], 2),
+    new Spurning('In computing what is Ram short for?', ['Really Awesome Memory', 'Random Access Memory', 'Rapid Attribute Memory', 'Rapid Access Memory'], 1)
+];
+
+
+   var container = document.getElementById('main');
+
+    container.innerHTML = container.innerHTML + spurningar[2].template();
+
