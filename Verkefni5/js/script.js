de95e98a5ef72af67cfc4b7ee31db0f7a36601dc
edvaldatli <eddi1122@hotmail.com>
2016-04-09 09:32:35 +0000

    Moments á íslensku

--- Verkefni5/js/script.js
+++ Verkefni5/js/script.js
@@ -1,4 +1,4 @@
-var mynd;
+moment().locale('is');
 
 $.ajax({
     'url': 'http://apis.is/concerts',
@@ -6,17 +6,18 @@ $.ajax({
     'dataType': 'json',
     'success': function(response) {
         for(var i = 0; i < response.results.length; i++) {
+
             $('.vidburdir').append('<div class="col s12 m6 l4"><div class="card">' +
                 '<div class="card-image waves-effect waves-block waves-light">' +
                 '<img class="activator" src="' + response.results[i].imageSource + '">' +
                 '</div>' +
                 '<div class="card-content">' +
                 '<span class="card-title activator grey-text text-darken-4 titill">' + response.results[i].eventDateName +'</span>' +
-                '<span class="titill">' + response.results[i].eventHallName +'</span>' +
+                '<span class="titill"><i class="material-icons">location_on</i>' + response.results[i].eventHallName +'</span>' +
                 '</div>' +
                 '<div class="card-reveal">' +
                 '<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>' + response.results[i].eventDateName + '</span>' +
-                '<p>Here is some more information about this product that is only revealed once clicked on.</p>' +
+                '<p>' + moment(response.results[i].dateOfShow).format('LLLL') + '</p>' +
                 '</div>' +
                 '</div>' +
                 '</div>');
