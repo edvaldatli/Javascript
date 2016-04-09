d0d13b8bc70da06b083059d5aedac2d69341b2f6
edvaldatli <eddi1122@hotmail.com>
2016-04-09 09:36:44 +0000

    Update

--- Verkefni5/js/script.js
+++ Verkefni5/js/script.js
@@ -1,12 +1,9 @@
-moment().locale('is');
-
 $.ajax({
     'url': 'http://apis.is/concerts',
     'type': 'GET',
     'dataType': 'json',
     'success': function(response) {
         for(var i = 0; i < response.results.length; i++) {
-
             $('.vidburdir').append('<div class="col s12 m6 l4"><div class="card">' +
                 '<div class="card-image waves-effect waves-block waves-light">' +
                 '<img class="activator" src="' + response.results[i].imageSource + '">' +
