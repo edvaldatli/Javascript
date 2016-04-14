52b2d3bc32fae243ce1292de3802c7bac2779543
edvaldatli <eddi1122@hotmail.com>
2016-04-14 20:31:38 +0000

    Update

--- Verkefni5/js/ajax.js
+++
@@ -1,8 +0,0 @@
-$.ajax({
-    'url': 'http://apis.is/concerts',
-    'type': 'GET',
-    'dataType': 'json',
-    'success': function(response) {
-        console.log($.parseJSON(response));
-    }
-});
